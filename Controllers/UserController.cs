1f659d04069203e428626a8623f90f8e38024b38
DimasVeliz <trevor90094@yandex.com>
2022-05-02 16:10:45 +0200

    UsersController created

---
+++ Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SuvillianceSystem.Connections.Infrastructure;
+using SuvillianceSystem.Models;
+
+namespace SuvillianceSystem.Authentication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        public ICRUD<User> UsersRepo { get; }
+        
+        public UserController(ILogger<UserController> logger,ICRUD<User> _usersRepo)
+        {
+            _logger = logger;
+            UsersRepo = _usersRepo;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
